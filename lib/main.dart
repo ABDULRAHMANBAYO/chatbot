96b4c9e17c087b7794edf55a5457d0066d4986ae
SULTAN <ramhziworld@yahoo.com>
2019-08-04 11:32:25 +0100

    Positioned messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,15 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Chat bot",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Chat bot'),
@@ -36,51 +28,60 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   //final List<ChatMessage> _messages = new List();
-  TextEditingController sentmessage;
-  int _counter = 0;
+  final TextEditingController sentmessage = TextEditingController();
 
-  void _submit() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _submit() {
+    debugPrint(sentmessage.text);
+    sentmessage.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
         body: new Container(
-          child: Row(
+          child: Column(
             children: <Widget>[
-Expanded(child: Text("How are you today?"
-
-),),
-
-              Expanded(
-                  child: TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter message",
-                          suffixIcon: Icon(Icons.send),
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black12),
-                              borderRadius: BorderRadius.circular(20)))))
+              Expanded(child: userMessage(context)),
+              Expanded(child: responseMessage(context)),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextFormField(
+                          controller: sentmessage,
+                          decoration: InputDecoration(
+                              hintText: "Enter message",
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black12),
+                                  borderRadius: BorderRadius.circular(20))))),
+                  IconButton(icon: Icon(Icons.send), onPressed: _submit)
+                ],
+              )
             ],
           ),
         ));
   }
+}
+
+Widget userMessage(BuildContext context) {
+  return Row(
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.only(left: 300.0),
+        child: Text("How are you???"),
+      )
+    ],
+  );
+}
+
+Widget responseMessage(BuildContext context) {
+  return Row(
+    children: <Widget>[
+      Container(
+          margin: EdgeInsets.only(right: 100.0),
+          child: Text("I am fine,how are you? "))
+    ],
+  );
 }
