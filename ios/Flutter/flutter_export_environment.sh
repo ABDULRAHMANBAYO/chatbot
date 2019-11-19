0f0caff41eca670da48d21ad0618b5f67f090a13
SULTAN <ramhziworld@yahoo.com>
2019-11-19 10:16:57 +0100

    Display all message from user

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/owner/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/owner/Desktop/chat_bot"
-export "FLUTTER_TARGET=/Users/owner/Desktop/chat_bot/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/owner/Desktop/chatbot"
+export "FLUTTER_TARGET=/Users/owner/Desktop/chatbot/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/owner/Desktop/flutter/bin/cache/artifacts/engine/ios"
--- lib/main.dart
+++ lib/main.dart
@@ -29,49 +29,111 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List _userMessages = new List();
+  final List _responseMessage = new List();
   final List _messages = new List();
-  var message="How are you";
-  final TextEditingController sentmessage = TextEditingController();
+  var message = "hi";
+  final userMessageController = TextEditingController();
 
   _submit() {
-    debugPrint(sentmessage.text);
-  setState(() {
-     message = sentmessage.text;
-  });
-    _messages.add(sentmessage.text);
-    print("Element in the array:$_messages");
-    _messages.clear();
-    sentmessage.clear();
+      message = userMessageController.text;
+      _userMessages.add(message);
+     //print(_userMessages); 
+     userMessageController.clear();
+   setState(() {
+    
+      
+      
+    }); 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: new Container(
-          child: Column(
-            children: <Widget>[
-              Expanded(child: userMessage(context)),
-              Expanded(child: responseMessage(context)),
-              enterMessage(context),
-            ],
-          ),
-        ));
-  }
-
-  Widget userMessage(BuildContext context) {
-    return Row(
+    final enterMessage = Row(
       children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(left: 300.0),
-          child: Text(message),
-          decoration: new BoxDecoration(color: Colors.blueAccent),
-        )
+        Expanded(
+            child: TextFormField(
+                controller: userMessageController,
+                decoration: InputDecoration(
+                    hintText: "Enter message",
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black12),
+                        borderRadius: BorderRadius.circular(20))))),
+        IconButton(icon: Icon(Icons.send), onPressed: _submit)
       ],
     );
+
+    final userMessage = Row(
+      children: <Widget>[
+        new Expanded(
+            child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: _userMessages.length,
+          itemBuilder: (BuildContext context, int index) {
+            return new Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                elevation: 4.0,
+                margin: EdgeInsets.only(left: 300.0),
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: new ListTile(
+                    title: Text(_userMessages[index]),
+                  ),
+                )
+                //decoration: new BoxDecoration(color: Colors.blueAccent),
+                );
+          },
+        ))
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+          child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height >= 775.0
+            ? MediaQuery.of(context).size.height
+            : 775.0,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            userMessage,
+            Expanded(
+              flex: 1,
+              child:Padding(
+              padding: EdgeInsets.only(top: 450, right: 40.0, left: 30.0),
+              child: 
+              enterMessage,
+            ), )
+            
+          ],
+        ),
+      )),
+      // Positioned(
+      //   top: 680,
+      //   right: 40,
+      //   child: sendButton,
+      // )
+    );
   }
+
+  // Widget userMessage(BuildContext context) {
+  //   return Row(children: <Widget>[
+  //     ListView.builder(
+  //       // padding: EdgeInsets.only(right:8.0),
+  //       itemCount: _userMessages.length,
+  //       itemBuilder: (BuildContext context, int index) {
+  //         return Text(userMessageController.text);
+  //       },
+  //     )
+  //   ]);
+  // }
 
   Widget responseMessage(BuildContext context) {
     return Row(
@@ -82,22 +144,6 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Text("I am fine and You???"),
           decoration: new BoxDecoration(color: Colors.blueGrey),
         )
-      ],
-    );
-  }
-
-  Widget enterMessage(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-            child: TextFormField(
-                controller: sentmessage,
-                decoration: InputDecoration(
-                    hintText: "Enter message",
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black12),
-                        borderRadius: BorderRadius.circular(20))))),
-        IconButton(icon: Icon(Icons.send), onPressed: _submit)
       ],
     );
   }
